ab8c4337b52572eed4f77ee1c730a16e75dac54a
Fachry Dennis Heraldi <fachrydennisheraldi@gmail.com>
2022-03-29 11:31:34 +0700

    Update progress basic puzzle operation

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from bnb import *
+
+mat = Puzzle()
+mat.load_puzzle('../test/reachable1.txt')
+# mat.print_puzzle()
+mat.print_kurang_i_plus_x()
+
+# for i in range(16):
+#     print(f"ubin {i+1} ada di {mat.find_ubin_position(i+1)}")
+
